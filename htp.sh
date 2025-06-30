04c2c9863ea9447aaab3cce0f5ee9d77ad2b58e4
Ahmed <achie@tutanota.com>
2025-06-30 17:21:22 +0300

    An almost complete rewritting of the script
    - Adding the ability to only enter three arguments for the old plates
    that does not have characters.
    - Improved arguments entering, making it more fail-proof
    - Improved help command
    - Improved transliteration
    - Adding request failure handling

--- htp.sh
+++ htp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Hawler Traffic Police Fines Checker
 # written by @mastawbeqezwan on github
 
-# basic colors
+#  colors
 RED='\033[31;1m'
 BLUE='\033[34;1m'
 GREEN='\033[32;1m'
@@ -11,13 +12,8 @@ MAGENTA='\033[35;1m'
 CYAN='\033[36;1m'
 RESET='\033[m'
 
-VERSION="1.00"
-
-PLATE_NO=""
-PLATE_CHAR=""
-REG_NO=""
-VEHICLE_CATEGORY=""
-VEHICLE_CATEGORY_PRESENTABLE=""
+# User-Agent
+VERSION="1.01"
 
 # Define user-agent
 USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0"
@@ -32,25 +28,73 @@ sorani_to_latin() {
         -e 's/ڵ/ll/g' -e 's/م/m/g' -e 's/ن/n/g' -e 's/ه‌/e/g' -e 's/ه/h/g' \
         -e 's/و/w/g' -e 's/ۆ/o/g' -e 's/ۇ/u/g' -e 's/ی/y/g' -e 's/ێ/ê/g' \
         -e 's/ە/e/g' -e 's/ئ//g' -e 's/ة/e/g' -e 's/ن/n/g' -e 's/ى/y/g' \
-        -e 's/\t/ /g'
+        -e 's/ \+/ /g'
 }
 
 show_help() {
-    echo "Usage: ./htp.sh [options]"
-    echo "./htp.sh <Vehicle Category> <Plate Character> <Plater Number> <Registration Number>"
-    echo
-    echo "Options:"
-    echo "  -h, --help        Show this help message and exit"
-    echo "  -t, --type        Show available vehicle types"
-    echo "  -v                Show script version"
-    echo
-    echo "Example:"
-    echo "  ./htp.sh private F 123 0123456"
-    echo "  ./htp.sh commericial - 123 0123456"
-    echo "  ./htp.sh m A 123 0123456"
+    cat <<EOF
+Usage: $0 [options] <Vehicle Type> [<Plate Character>] <Plate Number> <Registration Number>
+
+Options:
+  -h, --help        Show this help message and exit
+  -v                Show script version
+
+Vehicle Type:
+  p | private
+  r | rental
+  l | load
+  a | agricultural
+  c | commercial
+  m | motorcycle
+
+Examples:
+  $0 private F 123 0123456
+  $0 commercial B 123 0123456
+  $0 motorcycle 123 0123456     (without Plate Character)
+EOF
+}
+
+map_vehicle_type() {
+    local input="$1"
+
+    case "$input" in
+        private|p)
+            vehicle_type="1"
+            vehicle_type_text="Private"
+            ;;
+        rental|r)
+            vehicle_type="2"
+            vehicle_type_text="Rental"
+            ;;
+        load|l)
+            vehicle_type="3"
+            vehicle_type_text="Load"
+            ;;
+        agricultural|a)
+            vehicle_type="4"
+            vehicle_type_text="Agricultural"
+            ;;
+        commercial|c)
+            vehicle_type="5"
+            vehicle_type_text="Commercial"
+            ;;
+        motorcycle|m)
+            vehicle_type="6"
+            vehicle_type_text="Motorcycle"
+            ;;
+        *)
+            echo "Error: Invalid vehicle type '$input'"
+            exit 1
+            ;;
+    esac
 }
 
 function fetch_data {
+    local plate_char="$1"
+    local plate_char_text=$([ $plate_char = "0" ] && echo "" || echo "$plate_char ")
+    local plate_number="$2"
+    local registration_number="$3"
+   
     COOKIE_JAR=$(mktemp)
     curl -s -c "$COOKIE_JAR" -A "$USER_AGENT" "https://htp.moi.gov.krd/fines_form.php" >/dev/null
 
@@ -59,109 +103,105 @@ function fetch_data {
         -H "Referer: https://htp.moi.gov.krd/fines_form.php" \
         -H "Origin: https://htp.moi.gov.krd" \
         -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
-        --data "Sinif=${VEHICLE_CATEGORY}&plate=${PLATE_NO}&PlateChar=${PLATE_CHAR}&SanNumber=${REG_NO}" \
+        --data "Sinif=${vehicle_type}&plate=${plate_number}&PlateChar=${plate_char}&SanNumber=${registration_number}" \
         "https://htp.moi.gov.krd/fines_form_data_1.php")
 
-    #echo "$RESULT"
-    echo -e "Vehicle Category:      ${CYAN}$VEHICLE_CATEGORY_PRESENTABLE${RESET}"
-    echo -e "Plate Number:          ${GREEN}$2 $PLATE_NO${RESET}"
-    echo -e "Registration Number:   ${MAGENTA}$REG_NO${RESET}"
-
-    if awk -v result="$RESULT" 'BEGIN { 
-        if (result ~ /<div class=".*?<\/span> هیچ سزایه‌كى له‌سه‌ر نیه‌<\/h5><\/div>/) 
-            exit 0; 
-        else 
-            exit 1 
-        }'; then
-        echo -e "\n${GREEN}No fines were found.${RESET}"
-    else
-        # total amount of fines
-        total_fine_amount=$(echo "$RESULT" | awk 'match($0, /<h5>ژماره‌ى سه‌رپێچى <.*>(.*)<\/span><\/h5>/, arr) {print arr[1]}')
-        total_fine=$(echo "$RESULT" | awk 'match($0, /<h5>بڕى گشتى سه‌رپێچیه‌كانى <.*>(.*)<\/span><\/h5>/, arr) {print arr[1]}')
-
-        fines=$(echo "$RESULT" | awk -v blue="$BLUE" \
-            -v green="$GREEN" \
-            -v yellow="$YELLOW" \
-            -v red="$RED" \
-            -v cyan="$CYAN" \
-            -v reset="$RESET" 'BEGIN {
-        RS="</tr>";
-        ORS="\n"; 
-        print "Date|Time|City|Violation|Fine|Location" }
-        {
-            if (match($0, /<tr style="font-size: 0\.8rem">\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*/, arr)) 
+    # Check if the curl command make a successful request
+    if [ "$?" -eq 0 ]; then
+        echo -e "Vehicle Type:          ${CYAN}$vehicle_type_text${RESET}"
+        echo -e "Plate Number:          ${GREEN}${plate_char_text}$plate_number${RESET}"
+        echo -e "Registration Number:   ${MAGENTA}$registration_number${RESET}"
+        
+        if echo "$RESULT" | grep -q 'هیچ سزایه‌كى له‌سه‌ر نیه‌'; then
+            echo -e "\n${GREEN}No fines were found.${RESET}"
+        else
+            # total amount of fines
+            total_fine_amount=$(echo "$RESULT" | awk 'match($0, /<h5>ژماره‌ى سه‌رپێچى <.*>(.*)<\/span><\/h5>/, arr) {print arr[1]}')
+            total_fine=$(echo "$RESULT" | awk 'match($0, /<h5>بڕى گشتى سه‌رپێچیه‌كانى <.*>(.*)<\/span><\/h5>/, arr) {print arr[1]}')
+    
+            fines=$(echo "$RESULT" | awk -v blue="$BLUE" \
+                -v green="$GREEN" \
+                -v yellow="$YELLOW" \
+                -v red="$RED" \
+                -v cyan="$CYAN" \
+                -v reset="$RESET" 'BEGIN {
+            RS="</tr>";
+            ORS="\n"; 
+            print "Date|Time|City|Violation|Fine|Location" }
             {
-                printf "%s%s%s|%s|%s%s%s|%s%s%s|%s%s%s|%s%s%s\n", \
-                blue, arr[1], reset, \
-                arr[7], \
-                green, arr[3], reset, \
-                yellow, arr[4], reset, \
-                red, arr[5], reset, \
-                cyan, arr[6], reset
-            }
-        }' | sorani_to_latin |
-            column -t -s '|' -o " ")
-        echo -e "Number of Fines:       ${YELLOW}$total_fine_amount${RESET}"
-        echo -e "Total Fine Amount:     ${RED}$total_fine${RESET}"
-
-        echo -e "\n$fines"
+                if (match($0, /<tr style="font-size: 0\.8rem">\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*/, arr)) 
+                {
+                    printf "%s%s%s|%s|%s%s%s|%s%s%s|%s%s%s|%s%s%s\n", \
+                    blue, arr[1], reset, \
+                    arr[7], \
+                    green, arr[3], reset, \
+                    yellow, arr[4], reset, \
+                    red, arr[5], reset, \
+                    cyan, arr[6], reset
+                }
+            }' | sorani_to_latin |
+                column -t -s '|' -o " ")
+            echo -e "Number of Fines:       ${YELLOW}$total_fine_amount${RESET}"
+            echo -e "Total Fine Amount:     ${RED}$total_fine${RESET}"
+    
+            echo -e "\n$fines"
+        fi
+    else
+        echo "Error: Failed to fetch data from HTP"
     fi
-
     rm -f "$COOKIE_JAR"
 }
 
-while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-    -h | --help)
-        show_help
-        exit 0
-        ;;
-    private | p | rental | r | load | l | agricultural | a | commercial | c | motorcycle | m)
-        if [ "$#" -eq 4 ] && [[ "$2" == [a-zA-Z-] ]] && [ "$3" -eq "$3" ] && [ "$4" -eq "$4" ]; then
-            VEHICLE_CATEGORY_PRESENTABLE=$(echo "$1" |
-                sed -E \
-                    -e 's/\b(private|p)\b/Private/g' \
-                    -e 's/\b(rental|r)\b/Rental/g' \
-                    -e 's/\b(load|l)\b/Load/g' \
-                    -e 's/\b(agricultural|a)\b/Agricultural/g' \
-                    -e 's/\b(commercial|c)\b/Commercial/g' \
-                    -e 's/\b(motorcycle|m)\b/Motorcycle/g')           
-            
-            VEHICLE_CATEGORY=$(echo "$1" |
-                sed -E -e 's/private|p/1/g' \
-                    -e 's/rental|r/2/g' \
-                    -e 's/load|l/3/g' \
-                    -e 's/agricultural|a/4/g' \
-                    -e 's/commercial|c/5/g' \
-                    -e 's/motorcycle|m/6/g')
-            
-            PLATE_CHAR=$([ "$2" = "-" ] && echo "0" || echo "$2")
-            PLATE_NO="$3"
-            REG_NO="$4"
-
-            fetch_data
-        else
-            show_help
-            exit 1
-        fi
-        exit
-        ;;
-    -t | --type)
-        echo -e "private | p\nrental | r\nload | l\nagricultural | a\ncommercial | c\nmotorcyle | m"
-        exit
-        ;;
-    -v)
-        echo "Version $VERSION"
-        exit
-        ;;
-    *)
+main() {
+    if [ "$#" -eq 0 ]; then
         show_help
         exit 1
-        ;;
-    esac
-done
+    fi
 
-if [ "$#" -eq 0 ]; then
-    show_help
-    exit 1
-fi
+    case "$1" in
+        -h|--help)
+            show_help
+            ;;
+        -v)
+            echo "Version: $VERSION"
+            ;;
+        *)
+            # Check argument count
+            if [ "$#" -eq 3 ]; then
+                plate_char="0"
+                plate_number="$2"
+                registration_number="$3"
+
+            elif [ "$#" -eq 4 ]; then
+                plate_char="$2"
+                plate_number="$3"
+                registration_number="$4"
+                
+                # Capitalize plate character 
+                plate_char="${plate_char^}"
+                
+                # Check if the entered plate character is an alphabet
+                ! [[ "$plate_char" =~ ^[A-Z]$ ]] && echo "Error: Plate character must be an alphabet." && exit 1
+            else
+                echo "Error: You must provide 3 or 4 arguments."
+                show_help
+                exit 1
+            fi
+            
+            # Validate numeric values
+            if ! [[ "$plate_number" =~ ^[0-9]+$ && "$registration_number" =~ ^[0-9]+$ ]]; then
+                echo "Error: Plate and registration numbers must be numeric."
+                exit 1
+            fi
+
+            # Map vehicle type
+            map_vehicle_type "$1"
+            
+            # Call fetch_data
+            fetch_data "$plate_char" "$plate_number" "$registration_number"
+            ;;
+    esac
+}
+
+# Entrypoint
+main "$@"
